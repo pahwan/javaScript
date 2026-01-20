61e5f3b7782a6abe7d992a840cd998fd7c020a5a
pahwan <73048568+pahwan@users.noreply.github.com>
2026-01-20 14:53:45 +0545

    Create firstTriviaBot.js

---
+++ firstTriviaBot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Pawan", botLocation="Bhaluhi", favoriteLanguage="javaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation +".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is easy to use without semicolon.";
+console.log(codingFact);
+
+codingFact = "The most popular language. is " + favoriteLanguage + " in the entire world";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation);
