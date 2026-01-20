e14124b9c1c11fac01fd4fb5d1499881f8e47a90
pahwan <73048568+pahwan@users.noreply.github.com>
2026-01-20 15:22:44 +0545

    Create sentence_maker.js
    day 1 of coding via freeCodeCamp JS

---
+++ sentence_maker.js
@@ -0,0 +1,19 @@
+let adjective ="big";
+let noun ="snake"; 
+let verb="played";
+let place="pokhara";
+let adjective2 ="over";
+let noun2="mouse";
+
+let firstStory =("Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".");
+console.log("First story: " + firstStory);
+
+adjective ="black";
+noun ="hero"; 
+verb="kiss";
+place="lake";
+adjective2 ="gently";
+noun2="girl";
+
+let secondStory= ("Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".");
+console.log("Second story: "+ secondStory);
